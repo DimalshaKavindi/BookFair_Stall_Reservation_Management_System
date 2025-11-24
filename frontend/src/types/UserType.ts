e167892f7232dd2876d3e3ab4e7c12de404e7d12
DimalshaKavindi <139705921+DimalshaKavindi@users.noreply.github.com>
2025-11-24 14:24:52 +0530

    Update UserType.ts

--- frontend/src/types/UserType.ts
+++ frontend/src/types/UserType.ts
@@ -14,19 +14,3 @@ export interface UserProfileResponse {
     success: boolean;
     user: UserProfile;
 }
-export interface User {
-  id: number;
-  email: string;
-  businessName: string;
-  contactPerson: string;
-  phone: string;
-  address: string;
-  role: "VENDOR" | "EMPLOYEE";
-  createdAt?: string;
-  updatedAt?: string;
-}
-
-export interface ProfileResponse {
-  success: boolean;
-  user: User;
-}
